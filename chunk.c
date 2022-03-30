e61423635bb1ce787b7361c311f6986dbc0bedfe
sesank.mallikarjuna <sesank.mallikarjuna@gmail.com>
2022-03-30 18:11:45 -0500

    updated chunk.c to show start and end times

--- chunk.c
+++ chunk.c
@@ -6,6 +6,8 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <math.h>
+
 
 int
 main(int argc, char** argv){
@@ -25,6 +27,45 @@ main(int argc, char** argv){
 	mission_start = argv[6];
 	mission_end = argv[7];
 	file_in_path = argv[1];
+
+	int h, m, number_of_minutes;
+	char* date_buff = malloc(sizeof(char)*10);
+	char* date_time = malloc(sizeof(char)*5);
+	int date_time_ind=0;
+	date_buff = strtok(argv[6],"-");
+
+	/* split input date */
+	while(date_buff!=NULL)
+	{
+		if(date_time_ind == 0)
+			date_time = date_buff;
+		else if (date_time_ind == 1)
+			h = atoi(date_buff);
+		else
+			m = atoi(date_buff);
+		date_time_ind++;
+		date_buff = strtok(NULL,"-");
+	}
+
+	/* populate month and day variables */
+	char* month = malloc(sizeof(char)*3);
+	char* day = malloc(sizeof(char)*3);
+	month[0] = date_time[0];
+	month[1] = date_time[1];
+	day[0] = date_time[2];
+	day[1] = date_time[3];
+
+	/* convert month and days from strings to ints */
+	int month_num = atoi(month);
+	int day_num = atoi(day);
+	
+	/* array for number of days in month */
+	int months[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	number_of_minutes = h*60+m;
+
+	/* for(int i = 0; i < 5; i++) { */
+	/* } */
+	/* exit(0); */
 
 	// check if directory exists
 	DIR* dir = opendir(dir_name);
@@ -69,9 +110,25 @@ main(int argc, char** argv){
 	int done = 0; // bool to break the loop
 
 	while(!done){
-		char out_file_name[250];
-		sprintf(out_file_name, "%s/%s_%s_%s-%d.wav", dir_name, orig_file_name, mission_start, mission_end, count);
+		int time_hour;
+		if(number_of_minutes >= 24*60) {
+			number_of_minutes = number_of_minutes-(24*60);
+			day_num++;
+			if(day_num > months[month_num]){
+				month_num += 1;
+				day_num = 1;
+			}
+		}
 
+		time_hour = floor(number_of_minutes / 60);
+		int time_minute = number_of_minutes % 60;
+		// printf("%02d-%02d_%02d:%02d\n", month_num, day_num, time_hour, time_minute);
+
+		char out_file_name[250];
+		sprintf(out_file_name, "%s/%s_%02d-%02d_%02d-%02d-%d.wav", dir_name, orig_file_name, time_hour, time_minute, (int)floor((number_of_minutes + 30) / 60), (int)floor((number_of_minutes + 30) % 60), count);
+
+
+		/* if the window goes over number of elements, change window size to number of remaining elements */
 		if(ind + window > num_original_samples){
 			window = num_original_samples - ind;
 			info.frames = num_original_samples - ind;
@@ -79,15 +136,17 @@ main(int argc, char** argv){
 			printf("Chunks written: %d\n", count);
 		}
 
+		/* copy current chunk of file and write to a file  */
 		memcpy(buffer, data + ind, window * sizeof(short));
-
 		SNDFILE* outfile = sf_open(out_file_name, SFM_WRITE, &info);
 		sf_write_short(outfile, buffer, window);
 		sf_write_sync(outfile);
 		sf_close(outfile);
 
+		/* move to the next chunk and increment time */ 
 		ind += window;
 		count++;
+		number_of_minutes += 30;
 	}
 	sf_close(file);
 }
